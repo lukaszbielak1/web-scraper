9022e2b06d6d37a2c1c524289589f8870b27e77f
Lukasz <lukasz.bielak1@gmail.com>
2022-10-04 21:04:02 +0200

    remove unnecessary part

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,10 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from pyspark.sql.types import StructType, StructField, StringType
+
 class Mazda_Cars:
+    header = StructType([
+    StructField('Model', StringType(), True),
+    StructField('Calendar Year Introduced', StringType(), True),
+    StructField('Current Model Introduced', StringType(), True),
+    StructField('Lift', StringType(), True),
+    StructField('Description', StringType(), True)])
+
     def __init__(self):
         self.response = requests.get('https://en.wikipedia.org/wiki/List_of_Mazda_vehicles')
         self.soup = BeautifulSoup(self.response.text, features="html.parser")
-    
     def get_all_cars(self):
         models_list = []
         table = self.soup.find('table', attrs={'class':'wikitable'})
@@ -16,7 +24,5 @@ class Mazda_Cars:
                 cols = [ele.text.strip() for ele in cols]
                 models_list.append([ele for ele in cols if ele])
         return models_list
-mzd  = Mazda_Cars()
-for m in mzd.get_all_cars():
-    print(m)
+
         
